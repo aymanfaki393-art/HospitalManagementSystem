e54e643d87f83f86671ca7876a33cfe6caa02e83
aymanfaki393-art <aymanfaki393@gmail.com>
2026-08-18 18:31:41 +0800

    added test objects for admin and doctor.

--- src/hospitalmanagementsystem/HospitalManagementSystem.java
+++ src/hospitalmanagementsystem/HospitalManagementSystem.java
@@ -2,12 +2,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
+
+
 package hospitalmanagementsystem;
 
 /**
  *
  * @author HP
  */
+
+import hms.model.Doctor;
+import hms.model.AdminStaff;
+
+
 public class HospitalManagementSystem {
 
     /**
@@ -15,6 +22,18 @@ public class HospitalManagementSystem {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+        Doctor doc1 = new Doctor("D001", "drAli", "1234", "Dr. Ali", "Dermatologist");
+        doc1.editProfile();
+        
+        AdminStaff admin1 = new AdminStaff("AD001", "ad_omar", "1234", "Omar");
+        System.out.println(admin1.getName());
+        admin1.editProfile();
+        
+        
+        
+}
+        
+
+    
     
 }
